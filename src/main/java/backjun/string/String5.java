9c4dec08ee7c26a51c6881c8d3c63f45f3b4402e
jyyoun1022 <jyyoun1022@naver.com>
2024-06-17 18:52:52 +0900

    문자열 더하기

---
+++ src/main/java/backjun/string/String5.java
@@ -0,0 +1,23 @@
+package backjun.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class String5 {
+    /**
+     * N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+     * 첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int count = Integer.valueOf(str);
+        int result = 0;
+
+
+
+        System.out.println(result);
+    }
+}
