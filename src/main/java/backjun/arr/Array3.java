873c41c1f0c81bca0d20c26e497473d9d7acafe7
jyyoun <jyyoun@aistudio.co.kr>
2024-06-20 19:44:45 +0900

    배열 최대값

---
+++ src/main/java/backjun/arr/Array3.java
@@ -0,0 +1,37 @@
+package backjun.arr;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Array3 {
+    /**
+     * 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+     * 예를 들어, 서로 다른 9개의 자연수
+     * 3, 29, 38, 12, 57, 74, 40, 85, 61
+     * 이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+
+        for (int i = 0; i < arr.length; i ++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int max = 0;
+        int index = 0;
+        int count = 0;
+
+        for (int value : arr) {
+            count++;
+            if (value > max) {
+                max = value;
+                index = count;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
