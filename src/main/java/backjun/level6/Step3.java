3e00a141ddfe8bd864a8df8818fdc4125198d77d
jyyoun1022 <jyyoun1022@naver.com>
2024-06-21 22:38:26 +0900

    별 찍기

---
+++ src/main/java/backjun/level6/Step3.java
@@ -0,0 +1,36 @@
+package backjun.level6;
+
+import java.io.*;
+
+public class Step3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= num; i++){
+            for(int j = 0; j < num-i; j++){
+                bw.write(' ');
+            }
+            for(int k = 0; k < i*2-1; k++){
+                bw.write('*');
+            }
+            bw.newLine();
+        }
+
+        for(int i = num-1; i >= 0; i--){
+            for(int j = 0; j < num-i; j++){
+                bw.write(' ');
+            }
+            for(int k = 0; k < i*2-1; k++){
+                bw.write('*');
+            }
+            bw.newLine();
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
