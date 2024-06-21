041c23f85be3bb33fda2d12a6e66cb08a034f340
jyyoun <jyyoun@aistudio.co.kr>
2024-06-21 18:43:22 +0900

    새싹

---
+++ src/main/java/backjun/level6/Step1.java
@@ -0,0 +1,14 @@
+package backjun.level6;
+
+public class Step1 {
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+        System.out.println(s);
+
+    }
+}
